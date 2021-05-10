70230517de93d1a0340356f75de83bfaacf7efde
Sebastian Herrera <seba3886@gmail.com>
2021-05-10 14:38:49 -0300

    second task finished

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,45 @@
 import ExpenseItem from './ExpenseItem';
+import { useState } from 'react';
+import ExpensesFilter from '../ExpenseFilter/ExpensesFilter';
 import './Expenses.css';
 import Card from '../UI/Card';
 
 const Expenses = (props) => {
-return(
-    <Card className="expenses">
-         <ExpenseItem
-      title={props.expenses[0].title}
-      amount={props.expenses[0].amount}
-      date={props.expenses[0].date}
-      />
-      <ExpenseItem
-      title={props.expenses[1].title}
-      amount={props.expenses[1].amount}
-      date={props.expenses[1].date}
-      />
-      <ExpenseItem
-      title={props.expenses[2].title}
-      amount={props.expenses[2].amount}
-      date={props.expenses[2].date}
-      />
-      <ExpenseItem
-      title={props.expenses[3].title}
-      amount={props.expenses[3].amount}
-      date={props.expenses[3].date}
-      />
-        </Card>
-)
+
+  const [dateFilter, setDateFilter] = useState();
+
+  const setDateFilterHandler = (event) => {
+    setDateFilter(event.target.value);
+    console.log(event.target.value);
+  }
+
+  return (
+    <div>
+      <ExpensesFilter onDateChange={setDateFilterHandler} />
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
+  )
 }
 
 export default Expenses;
